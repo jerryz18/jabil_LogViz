400896a25f5da1aba1f1caf71a6592603cec07d0
Jerry Zheng <91507126+jerryz18@users.noreply.github.com>
2023-02-28 15:20:46 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Table } from "./components/Table";
+
+function App() {
+  return (
+    <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+      />
+      <div className="container">
+        <Table />
+      </div>
+    </div>
+  );
+}
+
+export default App;
