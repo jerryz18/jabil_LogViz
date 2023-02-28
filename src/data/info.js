6bf534e4f8d5865d7a10b60b23dc1b9d8dde2807
Jerry Zheng <91507126+jerryz18@users.noreply.github.com>
2023-02-28 15:24:23 -0500

    Add files via upload

---
+++ src/data/info.js
@@ -0,0 +1,26 @@
+export const info = [
+    {
+      id: 1,
+      time1: "2023-02-06 19:49:34:749179",
+      time2: "2023-02-06 19:49:34:749179",
+      device: "radio",
+      event: "EVENT",
+      message: "Sample",
+    },
+    {
+      id: 2,
+      time1: "2023-02-07 19:49:34:749179",
+      time2: "2023-02-07 19:49:34:749179",
+      device: "satellite",
+      event: "WARNING",
+      message: "Sample",
+    },
+    {
+      id: 3,
+      time1: "2023-02-07 19:49:34:749179",
+      time2: "2023-02-07 19:49:34:749179",
+      device: "radio",
+      event: "ERROR",
+      message: "Sample",
+    },
+];
